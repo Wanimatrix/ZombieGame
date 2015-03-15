94f106f0d872741c462c07d6de890c0761a9951e
Wouter Franken <wouter.franken@gmail.com>
2015-03-15 12:42:36 +0100

    changed gitignore

--- ZombieCamServer/src/CONFIG.java
+++ ZombieCamServer/src/CONFIG.java
@@ -2,8 +2,8 @@
 public class CONFIG {
 
 	public static final int localport = 8083;
-	public static final String localip = "192.168.1.254";
-	public static final String zombieserverip = "192.168.1.254";
+	public static final String localip = "192.168.137.160";
+	public static final String zombieserverip = "192.168.137.1";
 	public static final int zombieserverport = 8082;
 	
 	public static final String cameraname = "room2cam";
